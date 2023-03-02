4b7f742ea29a6dcafc0a23d7f5c0ea5a7f8b9ad0
pugal dev <pugalkmc@gmail.com>
2023-03-02 22:52:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 CHATGPT_ENDPOINT = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
 # ChatGPT API key
-CHATGPT_API_KEY = "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"
+CHATGPT_API_KEY = os.getenv("TOKEN")
 
 # Telegram bot token
 TELEGRAM_BOT_TOKEN = "5325072620:AAF3z2mQqmpyfyM9tkbNgcDn0L7-kBaEJOw"
